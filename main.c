521cd524073e12b611b79f959cccf1bfe1c22fa1
nimai <nimai.m.patel@gmail.com>
2024-11-16 17:40:16 -0500

    bar

---
+++ main.c
@@ -0,0 +1,27 @@
+#include "mm.h"
+#include "memlib.h"
+#include <stdio.h>
+
+int
+main(void)
+{
+    mem_init();
+
+    mm_init();
+
+    int *nice;
+    nice = mm_malloc(sizeof(*nice));
+    printf("pointer nice = 0x%lx\n", (unsigned long)(nice));
+    mm_free(nice);
+    nice = mm_malloc(sizeof(*nice));
+    printf("pointer nice = 0x%lx\n", (unsigned long)(nice));
+    nice = mm_malloc(sizeof(*nice));
+    printf("pointer nice = 0x%lx\n", (unsigned long)(nice));
+    nice = mm_malloc(sizeof(*nice));
+    printf("pointer nice = 0x%lx\n", (unsigned long)(nice));
+    nice = mm_malloc(sizeof(*nice));
+    printf("pointer nice = 0x%lx\n", (unsigned long)(nice));
+
+    mem_deinit();
+    return 0;
+}
